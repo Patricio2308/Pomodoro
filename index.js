b84199b773449764b3d627b0fe5127cb5e732d4c
Patricio2308 <patricioandres00@gmail.com>
2023-02-24 18:41:25 -0300

    button panel

---
+++ index.js
@@ -0,0 +1,12 @@
+var playBtn = document.getElementById('play') ;
+var pauseBtn = document.getElementById('pause') ;
+var nextBtn = document.getElementById('next') ;
+
+function play() {
+    console.log("works")
+}
+
+playBtn.addEventListener("click",play);
+pauseBtn.addEventListener("click",play);
+nextBtn.addEventListener("click",play);
+
