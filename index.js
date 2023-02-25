d538239efbba56394005fd8ee8532d96d9bf3f0a
Patricio2308 <patricioandres00@gmail.com>
2023-02-24 23:06:03 -0300

    countdown logic

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+
 var playBtn = document.getElementById('play') ;
 var pauseBtn = document.getElementById('pause') ;
 var nextBtn = document.getElementById('next') ;
+var interval;
 
-function play() {
-    console.log("works")
+let min = 0; 
+let sec = 10;
+ 
+
+const timeFormat = (num) => {
+    return num < 10 ? `0${num}` : num
 }
 
-playBtn.addEventListener("click",play);
-pauseBtn.addEventListener("click",play);
-nextBtn.addEventListener("click",play);
+const timeContDown = () => {
+    if (sec === 0){
+        min--
+        sec = 60
+    } else if(min >= 0) {
+        sec--
+    }
+    console.log(`${timeFormat(min)} : ${timeFormat(sec)}`) 
+    
+}
 
+const play = () => {
+    playBtn.removeEventListener("click",play);
+    
+    interval = setInterval(() => {
+        timeContDown();
+        if(min === 0 && sec === 0)
+        pause();
+    },1000);
+}
+
+const pause = () => {
+    clearInterval(interval)
+}
+    
+    playBtn.addEventListener("click",play);
+    pauseBtn.addEventListener("click",pause);
+    nextBtn.addEventListener("click",play);
+
+    
+        
