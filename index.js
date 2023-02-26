5b118e1568887d3b1125d93c06be463675de4f4e
Patricio2308 <patricioandres00@gmail.com>
2023-02-25 22:54:57 -0300

    bell sound

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ let min = 0;
 let sec = 10;
 let workInterval = 0;
 let breakTime = false;
- 
+
+function audioPlay(){
+    let sound = new Audio('./sounds/bell.wav');
+    sound.play()
+}
 
 const timeFormat = (num) => {
     return num < 10 ? `0${num}` : num
@@ -26,6 +30,7 @@ const printNumber = () => {
 
 const countIntervals = () => {
     pause();
+    audioPlay();
     workInterval++;
     breakTime = !breakTime;
     sec = 0;
@@ -45,7 +50,7 @@ const play = () => {
 }
 
 const pause = () => {
-    clearInterval(interval)
+    clearInterval(interval);
     playBtn.addEventListener("click",play);
     console.log(`Intervalo numero ${workInterval}, tiempo de descanso es ${breakTime} y el tiempo: 
     ${timeFormat(min)}:${timeFormat(sec)}`)
