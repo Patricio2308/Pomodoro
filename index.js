047c7599cd6ba2ccef8284570a2eb498d44695e8
Patricio2308 <patricioandres00@gmail.com>
2023-06-04 04:12:17 -0300

    storing option values

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ window.onload = function () {
     var optionBtn = document.querySelector(".options");
     var interval;
 
-    let min = 25;
     let pomoMin = 25;
     let restMin = 5;
     let longRestMin = 15;
@@ -22,20 +21,42 @@ window.onload = function () {
     let workInterval = 1;
     let automaticPlay = false;
     let breakTime = false;
+    
 
+    const timeFormat = (num) => {
+        return num < 10 ? `0${num}` : num
+    }
+
+    /* Setea los valores del localStorage a las etiquetas */
+    const setInitialsValueOption = (id1, id2, id3, id4) => {
+        document.getElementById(id1).value = pomoMin;
+        document.getElementById(id2).value = restMin;
+        document.getElementById(id3).value = longRestMin;
+        document.getElementById(id4).checked = automaticPlay;
+    }
+
+    /* Carga los valores iniciales desde la localStorage*/
+    const loadOptions = () => {
+        pomoMin = parseInt(localStorage.getItem('Pmin'));
+        restMin = parseInt(localStorage.getItem('Rmin'));
+        longRestMin = parseInt(localStorage.getItem('LRmin'));
+        automaticPlay = localStorage.getItem('AutoPlay') == 'true';
+        setInitialsValueOption("pomodoroTIme", "restTime", "longRestTime", "autoPlay");
+        document.getElementById("realTime").textContent = `${timeFormat(pomoMin)}:${timeFormat(sec)}`;
+    }
+    
+    loadOptions();
+    let min = pomoMin;
+    
     const sounds = {
         play: "./sounds/bell.wav",
         next: "./sounds/click.wav"
-    }
+    };
 
     function audioPlay(currentSound) {
         let sound = new Audio(currentSound);
         sound.volume = 0.4;
         sound.play();
-    }
-
-    const timeFormat = (num) => {
-        return num < 10 ? `0${num}` : num
     }
 
     const timeCountDown = () => {
@@ -63,6 +84,7 @@ window.onload = function () {
         });
     };
 
+    /* Calcula que clase de break time se asigna */
     const timeAllocation = () => {
         sec = 0;
         return !breakTime ? min = pomoMin : workInterval % 6 === 0 && breakTime ?
@@ -119,15 +141,39 @@ window.onload = function () {
             modal.style.display = "none";
     }
 
+    /* const setNewValueOption = (id1,id2,id3,id4) => {
+        document.getElementById(id1).value = pomoMin;
+        document.getElementById(id2).value = restMin;
+        document.getElementById(id3).value = longRestMin;
+        document.getElementById(id4).checked = automaticPlay;
+        console.log("seteado")
+    }
+ */
+    const chargeValueOption = (id) => {
+        console.log("cargado "+ id)
+        return document.getElementById(id).value
+    }
+
     function saveOptions() {
-        pomoMin = document.getElementById("pomodoroTIme").value;
-        restMin = document.getElementById("restTime").value;
-        longRestMin = document.getElementById("longRestTime").value;
-        let autoValue = document.getElementById("autoPlay").checked;
-        if (autoValue) { automaticPlay = true } else automaticPlay = false;
+        pomoMin = chargeValueOption("pomodoroTIme");
+        restMin = chargeValueOption("restTime");
+        longRestMin = chargeValueOption("longRestTime");
+        automaticPlay = document.getElementById("autoPlay").checked;
+        console.log(document.getElementById("autoPlay").checked)
+
         timeAllocation();
         printNumber();
         modalDisplay();
+        setInitialsValueOption("pomodoroTIme", "restTime", "longRestTime", "autoPlay")
+        storageOptions(pomoMin, restMin, longRestMin, automaticPlay);
+    }
+
+    /* Storage */
+    function storageOptions(pomoMin, restMin, longRestMin, automaticPlay) {
+        localStorage.setItem('Pmin',pomoMin);
+        localStorage.setItem('Rmin',restMin);
+        localStorage.setItem('LRmin',longRestMin);
+        localStorage.setItem('AutoPlay', automaticPlay);
     }
 
     /* Events */
